64585f94941996dced842fdf6cc5e0787c25c67e
Mustafa-Romanov <alboriy707@gmail.com>
2024-10-30 15:29:45 +0500

    app degi h1 ni ochirvordim

--- src/pages/Abdulboriy-Ibrohim/MainUni.jsx
+++ src/pages/Abdulboriy-Ibrohim/MainUni.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
-import './App.css'
 import { LineChart } from '@mui/x-charts'
-import {Route, Routes, NavLink } from 'react-router-dom';
+// import {Route, Routes, NavLink } from 'react-router-dom';
 
 
 
