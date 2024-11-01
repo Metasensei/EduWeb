231963f51d97a9dca3e193700115487ed9acd7b8
Hojiakbar-ux <hrahmonov677@gmail.com>
2024-11-01 17:18:46 +1200

    restart

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import '../Admin/Admin.css'
+import {Routes, Route} from 'react-router-dom'
+import AdminList from './AdminList.jsx'
 
 const Admin = () => {
   return (
     <>
-
+    <Routes>
+      <Route path='/AdminList' element = {<AdminList/>} />
+    </Routes>
     </>
+    
   )
 }
 
